5f8f4cab332e2c0a7b546a7c3670bf9e6fc77c02
yangqcc <yangqc_ars@outlook.com>
2017-02-15 10:33:47 +0800

    事件传递

---
+++ js_event/EventUtil.js
@@ -0,0 +1,26 @@
+/**
+ * Created by yangqc on 2017/2/15.
+ */
+var EventUtil = {
+
+    addHandler: function (element, type, handler) {
+        if (element.addEventListener) {
+            //冒泡阶段处理事件
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+
+    removerHandler: function (element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
+        }
+    }
+};
